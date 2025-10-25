205ec5feeb5bfa4f2c489dd78949d936d9214515
Ananthakrishnan VK <krishnaananthvk@gmail.com>
2025-10-26 01:57:37 +0530

    removed location of user

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:bus_ellide_mobile/core/themes/theme.dart';
 import 'package:bus_ellide_mobile/injection_container.dart';
-import 'package:bus_ellide_mobile/presentation/blocs/location_bloc/location_bloc.dart';
 import 'package:bus_ellide_mobile/presentation/navigation/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initDI();
+  WidgetsFlutterBinding.ensureInitialized(); // needed for async in main
+  await initDI(); // initialize dependencies
   runApp(ProviderScope(child: BmtcApp()));
 }
 
-final locationBlocProvider = ChangeNotifierProvider((ref) => LocationBloc());
-
-class BmtcApp extends ConsumerWidget {
-  BmtcApp({super.key});
+class BmtcApp extends StatelessWidget {
+  const BmtcApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final locationBloc = ref.watch(locationBlocProvider);
-
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BMTC Live Tracker',
       theme: appTheme,
